36119486fdb7706966393a49744c2084330845d5
Lucas <lukeadina@gmail.com>
2018-04-15 01:58:10 -0600

    Added project

---
+++ diskInventory/diskInventory/Artists.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/DiskInventory.Master" AutoEventWireup="true" CodeBehind="Artists.aspx.cs" Inherits="diskInventory.Artists" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="card card-outline-secondary align-content-center text-center">
+        <div class="card-header text-center">
+            <h3 class="mb-0">Under Construction</h3>
+        </div>
+        <div class="card-body">
+            <form runat="server">
+                <img class="img-fluid" src="Images/pika.gif" alt="Under Construction" />
+                <h4>Sorry, this page is under construction. We are working as hard as we can!</h4>
+                <asp:Button CssClass="form-control" ID="Button1" OnClick="btnReturnToDefault_Click" Text="Return Home" runat="server" />
+            </form>
+        </div>
+    </div>
+</asp:Content>
---
+++ diskInventory/diskInventory/Borrowers.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace diskInventory
+{
+    public partial class Borrowers : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAddAnother_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("/Borrowers.aspx");
+        }
+
+        protected void btnReturn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("/Default.aspx");
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+            txtEmail.Text = "";
+            txtPhone.Text = "";
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (validateFields())
+            {
+                lblModalTitle.Text = "Successfully Added Borrower";
+                lblModalBody.Text = txtFirstName + " has been successfully added to the database. Please select an option.";
+                ScriptManager.RegisterStartupScript(Page, Page.GetType(), "myModal", "$('#myModal').modal();", true);
+                upModal.Update();
+            } else
+            {
+                txtErrorMessage.Text = "One or more fields might not have been filled.";
+            }
+        }
+
+        private bool validateFields()
+        {
+            bool isValid = true;
+            if (String.IsNullOrEmpty(txtFirstName.Text)) isValid = false;
+            if (String.IsNullOrEmpty(txtLastName.Text)) isValid = false;
+            if (String.IsNullOrEmpty(txtEmail.Text)) isValid = false;
+            if (String.IsNullOrEmpty(txtPhone.Text)) isValid = false;
+            return isValid;
+        }
+    }
+}
